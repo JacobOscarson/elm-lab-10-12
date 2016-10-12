8dc3aa4955222b86e0991940a9497bffb4252245
Jacob Oscarson <jacob@plexical.com>
2016-10-12 17:12:50 +0200

    Replicated the minimal example from elm-uuid

--- Main.elm
+++ Main.elm
@@ -1,7 +1,76 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Html exposing (..)
+import Uuid
+import Random.Pcg exposing (Seed, initialSeed, step)
+import Html.App exposing (programWithFlags)
+import Html exposing (Html, div, button, text)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { currentSeed : Seed
+    , currentUuid : Maybe Uuid.Uuid
+    }
+
+
+type Msg
+    = NewUuid
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewUuid ->
+            let
+                ( newUuid, newSeed ) =
+                    step Uuid.uuidGenerator model.currentSeed
+            in
+                ( { model
+                    | currentUuid = Just newUuid
+                    , currentSeed = newSeed
+                  }
+                , Cmd.none
+                )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        uuidText =
+            case model.currentUuid of
+                Nothing ->
+                    "No Uuid was created so far"
+
+                Just uuid ->
+                    "Current Uuid: " ++ Uuid.toString uuid
+    in
+        div []
+            [ button [ onClick NewUuid ] [ text "Create a new Uuid!" ]
+            , text uuidText
+            ]
+
+
+
+{- this init function takes a tuple of two ints that are handed over
+   in the initializiation code of our Elm app in the javascript code. It
+   uses these JS random values as the initial seed.
+-}
+
+
+init : Int -> ( Model, Cmd Msg )
+init seed =
+    ( { currentSeed = initialSeed seed
+      , currentUuid = Nothing
+      }
+    , Cmd.none
+    )
 
 
 main =
-    text "Hello?"
+    programWithFlags
+        -- using programWithFlags to get the seed values from JS
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
